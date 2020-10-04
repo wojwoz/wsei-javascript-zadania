8784367f2731849d796cc398698bd42ad5e5c3fe
wojwoz <61656116+wojwoz@users.noreply.github.com>
2020-10-04 21:31:57 +0200

    1

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+
+
+//zadanie 1
+
+console.log("Wojciech Woźniak");
+
+//zadanie 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// zadanie 3
+
+console.log("hello world");// - poprawne
+console.log(hello world); // błędny zapis 
+//console.log("Hello World); // błędny zapis
+
+
